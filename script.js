20ec9bf073c3f15cadd3e39daa127b18ed151d44
Harshkashyap48 <133206563+Harshkashyap48@users.noreply.github.com>
2024-01-13 10:39:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function resetForm() {
+    // Reset the input fields
+    document.getElementById("nameInput").value = "";
+    document.getElementById("gmailInput").value = "";
+    document.getElementById("passwordInput").value = "";
+}
+
+function submitForm() {
+    // Add your form submission logic here
+    alert("Form submitted!");
+}
